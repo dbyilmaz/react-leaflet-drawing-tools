d63b56a6ae7ccacf22128717308b1213f5d93940
Hossein Talebi <hossein.talebi@farmersedge.ca>
2017-12-07 15:52:08 -0700

    Adds constants

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-
+import Map from './map'
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <div>
+        <Map />
+      </div>
     );
   }
 }
---
+++ src/components/constants.js
@@ -0,0 +1,3 @@
+export const ARCGIS_ONLINE = '//server.arcgisonline.com/ArcGIS'
+export const MAP_URL_ESRI = `${ARCGIS_ONLINE}/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}.jpg`
+export const MAP_URL_DEFAULT = MAP_URL_ESRI
